a3089205efdcce99bfada0e906fbd2350f965780
VasilDinchev <vasildinchev@gmail.com>
2020-01-18 23:42:38 +0200

    "Adds package models and add classes user, credit and debit card."

---
+++ Solution/src/main/java/com/example/demo/models/card/CreditCard.java
@@ -0,0 +1,37 @@
+package com.example.demo.models.card;
+
+public class CreditCard {
+
+    private int id;
+
+    private String number;
+
+    private int status;
+
+    public CreditCard() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
---
+++ Solution/src/main/java/com/example/demo/models/user/User.java
@@ -0,0 +1,93 @@
+package com.example.demo.models.user;
+
+import com.example.demo.models.card.CreditCard;
+import com.example.demo.models.card.DebitCard;
+
+import java.util.Base64;
+
+public class User {
+
+    private String username;
+
+    private int enable;
+
+    private String email;
+
+    private String password;
+
+    private DebitCard debitCard;
+
+    private CreditCard creditCard;
+
+    private String phoneNumber;
+
+    private byte[] picture;
+
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getEnable() {
+        return enable;
+    }
+
+    public void setEnable(int enable) {
+        this.enable = enable;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public DebitCard getDebitCard() {
+        return debitCard;
+    }
+
+    public void setDebitCard(DebitCard debitCard) {
+        this.debitCard = debitCard;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPicture() {
+        return Base64.getEncoder().encodeToString(picture);
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+}
